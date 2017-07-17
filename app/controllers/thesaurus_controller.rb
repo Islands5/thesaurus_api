67a4b49e02ef73b8d86a63d334b0db1bdbe6c577
Ryotaro Goto <five.islands.04010613@gmail.com>
2017-07-17 22:51:36 +0900

    fix syntax error

--- app/controllers/thesaurus_controller.rb
+++ app/controllers/thesaurus_controller.rb
@@ -2,10 +2,9 @@ class ThesaurusController < ApplicationController
   before_action :set_thesaurus_client, only: [:show]
 
   def show
-    option = {
-      default_keywords_limit: 20,
-      word_class: params[:word_class]
-    }
+    option = {}
+    option.merge!(word_class: params[:word_class]) if params[:word_class]
+
     res = @client.search params[:term], option
     render json: res
   rescue StandardError => e
--- lib/thesaurus/client.rb
+++ lib/thesaurus/client.rb
@@ -8,17 +8,17 @@ module Thesaurus
   ALLOW_ACTION = ["verb", "noun", "adverb", "conjunction"]
   class Client
 
-    def initialize(args = {default_keywords_limit: 20, word_class: "verb"})
+    def initialize(**args)
       args.each do |key, value|
         instance_variable_set("@#{key}", value)
       end
     end
 
     # www.thesaurus.comから検索結果を取得する
-    # @param [Integer] default_keywords_limit
-    # @param [enum] word_class 
+    # @param [String] term
+    # @param [Hash] args {word_class: an ALLOW_ACTION}
     # @return [json_array] 
-    def search term, args={default_keywords_limit: 20}
+    def search term, **args
       encoded_term = URI.encode term
       uri = "#{API_ENDPOINT}/#{encoded_term}"
       uri += "/" + args[:word_class] if args[:word_class].present? && ALLOW_ACTION.include?(args[:word_class])
@@ -26,8 +26,8 @@ module Thesaurus
       parsed_res = Nokogiri::HTML.parse(res)
       words = parsed_res.css('.relevancy-list span.text')
       ret = []
-      words.each.with_index(1) do |word, i|
-        break if i > args[:default_keywords_limit]
+      puts words.class
+      words.each do |word|
         ret << word.children.text
       end
       ret.to_json
